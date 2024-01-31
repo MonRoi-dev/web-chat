a1e01fdd4b218224a0d0bfa1214058958df0cf2b
David Arutiunyan <monroi.dev@gmail.com>
2024-01-31 18:49:57 +0300

    nicnames added

--- app.mjs
+++ app.mjs
@@ -13,17 +13,33 @@ const io = new Server(server);
 app.use(router);
 
 io.on('connection', async (socket) => {
+    let name = '';
+    let uid;
+    socket.on('user join', async (nickname) => {
+        try {
+            name = await db.query(
+                'INSERT INTO users (nickname) VALUES ($1) RETURNING *',
+                [nickname]
+            );
+            uid = name.rows[0].id;
+            name = name.rows[0].nickname;
+            console.log(uid);
+        } catch (e) {
+            console.log(e);
+        }
+    });
+
     socket.on('chat message', async (msg) => {
         let result;
         try {
             result = await db.query(
-                'INSERT INTO messages (content) VALUES ($1) RETURNING *',
-                [msg]
+                'INSERT INTO messages (content, user_id) VALUES ($1, $2) RETURNING *',
+                [msg, uid]
             );
         } catch (e) {
             return console.log(e);
         }
-        io.emit('chat message', msg, result.lastId);
+        io.emit('chat message', name + ': ' + msg, result.lastId);
     });
     if (!socket.recovered) {
         try {
